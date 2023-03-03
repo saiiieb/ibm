3d1368a65eb8c3035cb5a472d6dfc3200ef0d927
Ken Li <110652364+saiiieb@users.noreply.github.com>
2023-03-04 01:30:45 +0800

    Update solutions.sql

--- Q3/solutions.sql
+++ Q3/solutions.sql
@@ -4,4 +4,5 @@ INNER JOIN category_article_mapping
 ON category_article_mapping.article_id = article.article_id
 INNER JOIN owners
 ON article.owner_id=owners.owner_id
-GROUP BY owner_id;
+GROUP BY owner_id
+ORDER BY different_category_count DESC;
